c34248ed1f464d6e69d198322da55ff50873da99
Tiến Dũng <75050912+Gken92k10820@users.noreply.github.com>
2023-12-14 01:45:49 +0700

    Update settings.py

--- url_shortener/settings.py
+++ url_shortener/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1b=nf#ep()y4t3qcnafm5e^=ledfj7fn!v$lexirdbtgsss%^n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
